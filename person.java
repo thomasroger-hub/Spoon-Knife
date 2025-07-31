2f5088db68bb48f58f586bf287bec41ff0ca4e08
thomasroger-hub <thomasroger88888@gmail.com>
2025-08-01 05:11:25 +0800

    这是我在idea上测试GitHub，创建person.java文件，并提交上传到disheng分支。

---
+++ person.java
@@ -0,0 +1,17 @@
+/**
+ * @Description :
+ * @Title : person
+ * @Author : Ogden
+ * @Date : 2025/8/1 5:07
+ * @Version : 1.0
+ */
+
+public class person {
+    public static void main(String[] args) {
+        System.out.println("jfhlksfjskjffslljfsfj");
+    }
+
+    public Integer method2(){
+        return 0;
+    }
+}
